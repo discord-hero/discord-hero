ca50270120719c0cad3f4fb1c7f51dce1cf36eb6
monospacedmagic <lucina@monospacedmagic.io>
2019-02-07 00:37:16 +0100

    Forgot a comma in setup.py

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: JavaScript',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3,
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
