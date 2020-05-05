6465b722fbf2439469d4e5fa9a6f850c2457bb88
monospacedmagic <luci@monospacedmagic.dev>
2020-05-05 16:07:10 +0200

    Let db.load return a Model after loading it

--- hero/db.py
+++ hero/db.py
@@ -12,23 +12,33 @@ class Database:
         self.core = core
 
         from hero.models import (User, Guild, TextChannel, VoiceChannel,
-                                 Role, Emoji, Member, Message)
+                                 CategoryChannel, Role, Emoji, Member, Message)
         self._model_map = {
             discord.User: User,
             discord.Guild: Guild,
             discord.TextChannel: TextChannel,
             discord.VoiceChannel: VoiceChannel,
+            discord.CategoryChannel: CategoryChannel,
             discord.Role: Role,
             discord.Emoji: Emoji,
             discord.PartialEmoji: Emoji,
             discord.Member: Member,
             discord.Message: Message
         }
+        self._models = tuple(self._model_map.values())
+        self._discord_classes = tuple(self._model_map.keys())
 
     async def load(self, discord_obj):
-        if isinstance(discord_obj, tuple(self._model_map.keys())):
+        if isinstance(discord_obj, self._discord_classes):
             cls = self._model_map[type(discord_obj)]
             obj, existed_already = await cls.from_discord_obj(discord_obj)
             return obj, existed_already
+        elif isinstance(discord_obj, self._models):
+            try:
+                await discord_obj.async_load()
+                existed_already = True
+            except discord_obj.__class__.DoesNotExist:
+                existed_already = False
+            return discord_obj, existed_already
         else:
             raise TypeError("obj has to be an object from Discord")
