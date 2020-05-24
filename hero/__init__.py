9249c7ff964728234f5ca647dc005c7f68c2fb4d
monospacedmagic <luci@monospacedmagic.dev>
2020-05-24 23:56:51 +0200

    Prepare for v0.1.0-alpha.6 release

--- hero/__init__.py
+++ hero/__init__.py
@@ -64,7 +64,7 @@ __author__ = 'monospacedmagic et al.'
 __license__ = 'Apache-2.0 OR MIT'
 __copyright__ = 'Copyright 2019 monospacedmagic et al.'
 __version__ = '0.1.0-alpha.6'
-__is_release__ = False
+__is_release__ = True
 
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial',
