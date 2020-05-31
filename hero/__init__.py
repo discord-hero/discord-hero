ad590d583d1aa8ea446be52af81d372c4bfd2d95
monospacedmagic <luci@monospacedmagic.dev>
2020-05-31 11:05:37 +0200

    Bump version to 0.1.0-beta.4

--- hero/__init__.py
+++ hero/__init__.py
@@ -76,8 +76,8 @@ __title__ = 'discord-hero'
 __author__ = 'monospacedmagic et al.'
 __license__ = 'Apache-2.0 OR MIT'
 __copyright__ = 'Copyright 2019-2020 monospacedmagic et al.'
-__version__ = '0.1.0-beta.3'
-__is_release__ = True
+__version__ = '0.1.0-beta.4'
+__is_release__ = False
 
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial',
